6281474e287cf4a3b0154e97e26f8709d4eb3d4e
Max Ma <xiao.ma_1@outlook.com>
2021-06-08 15:08:30 +0000

    learning about dictionaries

---
+++ challenge2/basic_challenge2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+icecream= ["flavors", "sour"]
+tlgclass= ["Brian","Clint","Damian","Dan","David","Jelani","Jerad","Jon","Jun","Mark","Max"]
+icecream.append(99)
+q1 = input("enter a number between 0 and 10:")
+q1_num = int(q1)
+print(f"{icecream[2]} {icecream[0]} and {tlgclass[q1_num]} chooses to be {icecream[1]}.")
