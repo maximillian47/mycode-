c77acd7b955a849f58b5f0996c7ffed32c7c4346
Max Ma <xiao.ma_1@outlook.com>
2021-06-10 20:29:10 +0000

    python and excel, including working with pandas

---
+++ project1/warriorQuest.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+if score == 9:
+    print("Jason Bourne, thank you for your service.")
+elif score == 8 or score == 7
+    print("Deplorable grunt, what's your ASVAB score?")
+elif score == 6 or score == 5
+    print("paint-ballerz, don't forget your face-shield.")
+elif score == 4 
+    print("Airsoft milsim, keep trying.")
+else :
+    print("Keyboard warrior, keep typing.")
