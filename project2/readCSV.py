89cb676b3f0e672733494210d11036c36b72c46a
Max Ma <xiao.ma_1@outlook.com>
2021-06-18 13:00:45 +0000

    p2

--- project2/readCSV.py
+++ project2/readCSV.py
@@ -1,5 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env
 
 import csv
 
+print("""
+        Welcome, let's do it.
+      """)
+
 with open("brett_comics.txt", "r") as comicfile:
+    comicDoc = csv.reader(comicfile, delimiter=",")
+    userChoice = input("""Choose 
+                    1.
+                    2. 
+                    3.
+                    >""").title()
+    
+    for line in comicDoc:    
+        if author in line.lower():
+                filename = "pokepoke.txt"
+
+                with open(filename, "w") as pkfile:
+                print(f'{line}', file=comicD)
+
+
